8b5037b535d66cf04828c338ce2711804f60c392
eakyoungyu <eakyoungyu@gmail.com>
2019-03-10 02:06:18 +0900

    갤러리에서 사진 불러오기

--- 20gosu_proj/app/src/main/java/com/example/a20gosu_proj/MainActivity.kt
+++ 20gosu_proj/app/src/main/java/com/example/a20gosu_proj/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.example.a20gosu_proj
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private var mainButtonGallery: Button? =null
+    companion object {
+        private val REQUEST_SELECT_IMAGE_IN_ALBUM = 0
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainButtonGallery = findViewById<View>(R.id.main_button_gallery) as Button
+        mainButtonGallery!!.setOnClickListener { selectImageInAlbum() }
+    }
+
+
+
+    fun selectImageInAlbum(){
+        val intent = Intent(Intent.ACTION_GET_CONTENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.type = "image/*"
+        if(intent.resolveActivity(packageManager)!=null){
+            startActivityForResult(intent, REQUEST_SELECT_IMAGE_IN_ALBUM)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(resultCode == Activity.RESULT_OK
+            && requestCode == REQUEST_SELECT_IMAGE_IN_ALBUM){
+            var intent = Intent(this, SelectImageActivity::class.java)
+            intent.data = data?.data
+            startActivity(intent)
+        }
     }
 }
---
+++ 20gosu_proj/app/src/main/java/com/example/a20gosu_proj/SelectImageActivity.kt
@@ -0,0 +1,58 @@
+package com.example.a20gosu_proj
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+
+class SelectImageActivity : AppCompatActivity() {
+    private var buttonOk :Button? = null
+    private var buttonCancel: Button? = null
+    private var imageView: ImageView? = null
+    private var fileUri: Uri? = null
+
+    companion object {
+        private val REQUEST_SELECT_IMAGE_IN_ALBUM = 1
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_image)
+
+        buttonOk = findViewById(R.id.selectImage_button_ok) as Button
+        buttonCancel = findViewById(R.id.selectImage_button_cancel) as Button
+        imageView = findViewById(R.id.selectImage_imageView) as ImageView
+
+        fileUri = intent?.data
+        imageView!!.setImageURI(fileUri)
+
+        buttonCancel!!.setOnClickListener { selectImageInAlbum() }
+        buttonOk!!.setOnClickListener { confirmImage() }
+    }
+
+
+    fun confirmImage(){
+        //다음 액티비티로 전환
+        Toast.makeText(this, "이미지 선택 완료", Toast.LENGTH_LONG).show()
+    }
+
+    fun selectImageInAlbum(){
+        val intent = Intent(Intent.ACTION_GET_CONTENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.type = "image/*"
+        if(intent.resolveActivity(packageManager)!=null){
+            startActivityForResult(intent, REQUEST_SELECT_IMAGE_IN_ALBUM)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(resultCode == Activity.RESULT_OK
+                && requestCode == REQUEST_SELECT_IMAGE_IN_ALBUM){
+            imageView!!.setImageURI(data?.data)
+        }
+    }
+}
