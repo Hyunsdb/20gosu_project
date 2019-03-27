93a637fba8b3c7f2f1cac4cf9c58a3eda34ade8b
eakyoungyu <eakyoungyu@gmail.com>
2019-03-27 19:55:40 +0900

    google cloud 요청 보내기

--- 20gosu_proj/app/src/main/java/com/example/a20gosu_proj/ResultImageActivity.kt
+++ 20gosu_proj/app/src/main/java/com/example/a20gosu_proj/ResultImageActivity.kt
@@ -4,6 +4,11 @@ import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.ImageView
+import com.example.a20gosu_proj.BuildConfig.ApiKey
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
 
 class ResultImageActivity : AppCompatActivity() {
     private var imageView: ImageView? = null
@@ -16,5 +21,38 @@ class ResultImageActivity : AppCompatActivity() {
 
         imageView = findViewById(R.id.resultImage_imageView)
         imageView!!.setImageURI(fileUri)
+        sendPost()
+    }
+    fun sendPost(){
+        val url = "https://vision.googleapis.com/v1/images:annotate?key=" + ApiKey
+        val client = OkHttpClient()
+        val JSON = MediaType.get("application/json; charset=utf-8")
+        val body= RequestBody.create(JSON, "{\n" +
+                "  \"requests\": [\n" +
+                "    {\n" +
+                "      \"image\": {\n" +
+                "        \"source\": {\n" +
+                "          \"imageUri\": \"gs://wearethegreatgosu/demo-image.jpg\"\n" +
+                "        }\n" +
+                "      },\n" +
+                "      \"features\": [\n" +
+                "        {\n" +
+                "          \"type\": \"OBJECT_LOCALIZATION\"\n" +
+                "        }\n" +
+                "      ]\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}")
+
+        Thread(){
+            val request = Request.Builder()
+                .url(url)
+                .post(body)
+                .build()
+
+            val response = client.newCall(request).execute()
+            println(response.request())
+            println(response.body()!!.string())
+        }.start()
     }
 }
